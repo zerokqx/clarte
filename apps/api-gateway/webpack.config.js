fbd9efa488ed750f34be05b596be0333bee9aab1
zerokqx <swiftkey382@gmail.com>
2026-06-18 21:13:17 +0300

    fix: generate package.json and install production deps for api-gateway

--- apps/api-gateway/webpack.config.js
+++ apps/api-gateway/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       assets: ['./src/assets'],
       optimization: false,
       outputHashing: 'none',
-      generatePackageJson: false,
+      generatePackageJson: true,
       sourceMap: true,
     }),
   ],
