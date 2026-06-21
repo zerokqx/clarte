cae79ba9bd98483ccac276f259406b0a397d509c
zerokqx <swiftkey382@gmail.com>
2026-06-22 02:36:02 +0300

    chore: переместить frontend в директорию apps/frontend
    - обновить пути в tsconfig.json
    - обновить ключи проектов в pnpm-lock.yaml
    - перенести конфигурацию vite.config.mts

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import '@mantine/core/styles.css';
 import '@mantine/tiptap/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
-import {App} from './app/app';
+import { App } from './app/app';
 import { MantineProvider } from '@mantine/core';
 
 const root = ReactDOM.createRoot(
