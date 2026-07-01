7137aa77c35d087fd0afb69cee47963d151805b7
zerokqx <swiftkey382@gmail.com>
2026-07-01 19:14:22 +0300

    refactor: удалить файл length.ts и вернуть экспорт только из helpers.ts

--- apps/frontend/src/shared/lib/mantine/index.ts
+++ apps/frontend/src/shared/lib/mantine/index.ts
@@ -1,10 +1,2 @@
-import * as helpers from './helpers';
-import * as length from './length';
-
 export * from './helpers';
-export * from './length';
-
-export const M = {
-  ...helpers,
-  ...length,
-};
+export * as M from './helpers';
