189321f8d229de0dad1870435981d49a78abb36e
zerokqx <swiftkey382@gmail.com>
2026-07-03 17:31:14 +0300

    feat: реализовать рекурсивный карри и добавить unit-тесты для @clarte/shared

--- packages/shared/eslint.config.mjs
+++ packages/shared/eslint.config.mjs
@@ -8,7 +8,11 @@ export default [
       '@nx/dependency-checks': [
         'error',
         {
-          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}'],
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/vitest.config.{js,ts,mjs,mts}',
+          ],
+          ignoredDependencies: ['vitest'],
         },
       ],
     },
--- packages/shared/src/functions/curry.ts
+++ packages/shared/src/functions/curry.ts
@@ -1,10 +1,19 @@
-export function curry<A, B, R>(fn: (a: A, b: B) => R): (a: A) => (b: B) => R;
-export function curry<A, B, C, R>(fn: (a: A, b: B, c: C) => R): (a: A) => (b: B) => (c: C) => R;
-export function curry<A, B, C, D, R>(
-  fn: (a: A, b: B, c: C, d: D) => R,
-): (a: A) => (b: B) => (c: C) => (d: D) => R;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type Curry<Args extends unknown[], Return> = Args extends []
+  ? () => Return
+  : <PartialArgs extends unknown[]>(
+      ...args: PartialArgs
+    ) => PartialArgs extends []
+      ? Curry<Args, Return>
+      : Args extends [...PartialArgs, ...infer Remaining]
+        ? Remaining extends []
+          ? Return
+          : Curry<Remaining, Return>
+        : Return;
 
-export function curry(fn: (...args: any[]) => any) {
+export function curry<Args extends unknown[], Return>(
+  fn: (...args: Args) => Return,
+): Curry<Args, Return> {
   return function curried(this: any, ...args: any[]): any {
     if (args.length >= fn.length) {
       return fn.apply(this, args);
@@ -12,5 +21,5 @@ export function curry(fn: (...args: any[]) => any) {
     return function (this: any, ...nextArgs: any[]) {
       return curried.apply(this, args.concat(nextArgs));
     };
-  };
+  } as unknown as Curry<Args, Return>;
 }
