af0aad5b8c727a1343e3bb836097750bb6eb3a3b
zerokqx <swiftkey382@gmail.com>
2026-07-07 03:14:09 +0300

    refactor: переименовать BottomSubActions в SubActions

--- apps/frontend/src/widgets/bottom-navigation/ui/bottom-navigation-sub-actions/bottom-navigation-sub-actions.tsx
+++
@@ -1,9 +0,0 @@
-import { Portal } from '@mantine/core';
-import { ReactNode } from 'react';
-
-export interface BottomSubActionsProps {
-  children: ReactNode;
-}
-export const BottomSubActions = ({ children }: BottomSubActionsProps) => {
-  return <Portal target={'#bottom-nav-sub-actions'}>{children}</Portal>;
-};
