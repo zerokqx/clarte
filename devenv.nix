c2cdd9d8008f07197b4dae51362538465f42f779
zerokqx <swiftkey382@gmail.com>
2026-09-05 16:12:34 +0300

    fix

--- devenv.nix
+++ devenv.nix
@@ -4,54 +4,24 @@
 }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
 
   packages = [
     pkgs.git
     pkgs.protobuf
     pkgs.openssl
     pkgs.minio-client
+    pkgs.cloc
   ];
 
   languages.javascript = {
     enable = true;
     pnpm.enable = true;
   };
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+  scripts.line-count.exec = " cloc ./apps ./packages  --exclude-dir=node_modules,build,dist,.next,out,.turbo,coverage";
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
 
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
